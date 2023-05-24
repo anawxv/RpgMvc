4a155e40ae6e34618bf1d019755d797efe684e0a
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-05-23 22:49:33 -0300

    Tem que terminar

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -80,5 +80,147 @@ namespace RpgMvc.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<ActionResult> DetailsAsync(int?id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuarip");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    PersonagemViewModel p = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<PersonagemViewModel>(serialized));
+                    return View(p);
+                }
+                else 
+                throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult> EditAsync (int? id)
+        {
+            try 
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    PersonagemViewModel p = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<PersonagemViewModel>(serialized));
+                    return View(p);
+                }
+                else 
+                throw new System.Exception(serialized);
+
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+    [HttpPost]
+    public async Task<ActionResult> EditAsync(PersonagemViewModel p)
+    {
+        try
+            {
+            HttpClient httpClient = new HttpClient();
+            string token = HttpContext.Session.GetString("SessionTokenUsuario");
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(p));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpResponseMessage response = await httpClient.PutAsync(uriBase, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                TempData["Mensagem"] =
+                    string.Format("Personagem {0}, classe {1} atualizado com sucesso!", p.Nome, p.Classe);
+
+                return RedirectToAction("Index");
+                }
+            else 
+            throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+            TempData["MensagemErro"] = ex.Message;
+            return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage response = await httpClient.DeleteAsync(uriBase + id.ToString());
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Personagem Id {0} removido com sucesso!", id);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DisputaGeralAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                string uriBuscaPersonagens = "http://ANAVIEIRA.somee.com/RpgApi/Personagens/GetAll";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuscaPersonagens);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                List<PersonagemViewModel> listaPersonagens = await Task.Run(() => JsonConvert.DeserializeObject<List<PersonagemViewModel>>(serialized));
+                return RedirectToAction("Index", "Personagens");
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index", "Personagens");
+            }
+        }
     }
 }
