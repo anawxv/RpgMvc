196fe0ad2a1c6ac7fa4fd8f72fb4acf80d0b3ea6
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-05-31 18:16:49 -0300

    Concluido

--- Controllers/PersonagemHabilidadesController.cs
+++ Controllers/PersonagemHabilidadesController.cs
@@ -67,5 +67,61 @@ namespace RpgMvc.Controllers
             }
             return RedirectToAction("Index", new {Id = personagemId});
         }
+
+        [HttpGet]
+        public async Task<ActionResult> CreateAsync(int id, string nome)
+        {
+            try
+            {
+                string  uriComplementar = "GetHabilidades";
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+                List<HabilidadeViewModel> habilidades = await Task.Run(() => JsonConvert.DeserializeObject<List<HabilidadeViewModel>>(serialized));
+                ViewBag.ListaHabilidades = habilidades;
+
+                PersonagemHabilidadesViewModel ph = new PersonagemHabilidadesViewModel();
+                ph.Personagem = new PersonagemViewModel();
+                ph.Habilidade = new HabilidadeViewModel();
+                ph.PersonagemId = id;
+                ph.Personagem.Nome = nome;
+
+                return View(ph);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Create", new { id, nome });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(PersonagemHabilidadesViewModel ph)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var content = new StringContent(JsonConvert.SerializeObject(ph));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    TempData["Mensagem"] = "Habilidade cadastrada com sucesso";
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+            }
+            return RedirectToAction("Index", new { id = ph.PersonagemId});
+        }
     }
 }
